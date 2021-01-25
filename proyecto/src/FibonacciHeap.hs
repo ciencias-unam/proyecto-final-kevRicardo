3888ee7af6ebb1a95c375fc9766e90a057a1e5dc
kevRicardo <kevin.ricardo@ciencias.unam.mx>
2021-01-25 11:40:08 -0600

    Metodos

--- proyecto/src/FibonacciHeap.hs
+++ proyecto/src/FibonacciHeap.hs
@@ -8,7 +8,7 @@ module FibonacciHeap where
     data BTree a = Node { rango :: Int,
                         raiz :: a,
                         hijos :: [BTree a] } 
-                        deriving (Eq, Show)
+                        deriving (Eq, Show, Ord)
 
     -- | Instancia Functor a Binary Tree
     instance Functor BTree where
@@ -29,7 +29,7 @@ module FibonacciHeap where
     data FHeap a = Empty | FHeap { tam :: Int,
                              mTree :: BTree a,
                              arboles :: [BTree a] }
-                             deriving (Eq, Show)
+                             deriving (Eq, Show, Ord)
 
     -- | Instancia Functor a Fibonacci Heap
     instance Functor FHeap where
@@ -41,4 +41,22 @@ module FibonacciHeap where
         pure a = FHeap 1 (Node 1 a []) []
 
         Empty <*> _ = Empty
-        (FHeap t m a) <*> fhy = m <*> fhy
+        --(FHeap t m a) <*> fhy = fmap m a
+
+    ordena :: (Ord a) => FHeap a -> FHeap a -> FHeap a
+    ordena Empty fh = fh
+    ordena fh Empty = fh
+    ordena (FHeap t1 m1 a1) (FHeap t2 m2 a2)
+        | raiz m1 < raiz m2 = FHeap (t1+t2) m1 (m2:a1++a2)
+        | otherwise = FHeap (t1+t2) m2 (m1: a1++a2)
+
+    inserta :: (Ord a) => FHeap a -> a -> FHeap a
+    inserta fib elem = ordena fib $ pure elem
+
+    liga :: (Ord a) => BTree a -> BTree a -> BTree a
+    liga t1@(Node ra1 r1 h1) t2@(Node ra2 r2 h2)
+        | r1 < r2 = Node (ra1+1) r1 (t2:h1)
+        | otherwise = Node (ra1+1) r2 (t1:h2)
+
+    minimo :: (Ord a) => FHeap a -> a
+    minimo = raiz . mTree
